d6c5b11078cb728c970111339d44fffcafbe7cca
davidwhitely <david.whitely@codeandtheory.com>
2015-05-05 19:29:29 -0400

    Removing child spawn.

--- server.js
+++ server.js
@@ -13,6 +13,6 @@ server.listen(process.env.PORT || 3000);
 server.on('listening', function() {
 	console.log('Express server started on port %s at %s', server.address().port, server.address().address);
 
-	var spawn = require('child_process').spawn
-	spawn('open', ['http://localhost:'+server.address().port]);
+//	var spawn = require('child_process').spawn
+//	spawn('open', ['http://localhost:'+server.address().port]);
 });
